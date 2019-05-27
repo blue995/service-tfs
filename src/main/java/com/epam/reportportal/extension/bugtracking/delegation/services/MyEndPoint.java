942621db5abb47a17b8b0c5fc3011325787be7d2
Tobias Blaufuß <t.blaufuss@gmx.de>
2019-05-27 11:25:01 +0200

    [UPDATE] log message

--- src/main/java/com/epam/reportportal/extension/bugtracking/delegation/services/MyEndPoint.java
+++ src/main/java/com/epam/reportportal/extension/bugtracking/delegation/services/MyEndPoint.java
@@ -26,6 +26,12 @@ public class MyEndPoint {
     @Value("rp.bts.tfs.project")
     private String tfsProject;
 
+    public MyEndPoint(){
+        System.out.println(1 + ": '" + microServiceUrl + "'");
+        System.out.println(2 + ": '" + tfsServerUrl + "'");
+        System.out.println(3 + ": '" + tfsProject + "'");
+    }
+
     @RequestMapping("/test/{id}")
     public Ticket hello(@PathVariable String id){
         RestTemplate tmpl = new RestTemplate();
